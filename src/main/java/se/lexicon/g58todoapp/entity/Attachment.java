57a4386f58946d5dbcc7355d75f447046d490bf3
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-27 10:59:37 +0100

    Implement Attachment

--- src/main/java/se/lexicon/g58todoapp/entity/Attachment.java
+++ src/main/java/se/lexicon/g58todoapp/entity/Attachment.java
@@ -1,6 +1,52 @@
 package se.lexicon.g58todoapp.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-// TODO IMPLEMENT
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Entity
+@Table(name = "attachments")
 public class Attachment {
+    @Id
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Column(nullable = false, length = 100)
+    private String fileName;
+
+    @NonNull
+    @Column(nullable = false, length = 100)
+    private String FileType;
+
+    private byte[] data;
+
+    public Attachment(@NonNull String fileName, @NonNull String fileType, byte[] data) {
+        this.fileName = fileName;
+        FileType = fileType;
+        this.data = data;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Attachment that = (Attachment) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }

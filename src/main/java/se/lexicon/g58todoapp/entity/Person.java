7ff0bb15be16fe35fc545986648cfca4f86aeaf6
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-27 11:09:56 +0100

    Add @CreationTimestamp for createdAt and Equals() + HashCode()

--- src/main/java/se/lexicon/g58todoapp/entity/Person.java
+++ src/main/java/se/lexicon/g58todoapp/entity/Person.java
@@ -3,8 +3,11 @@ package se.lexicon.g58todoapp.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 
 @Getter
@@ -12,7 +15,6 @@ import java.time.LocalDate;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @RequiredArgsConstructor
 @ToString
-
 @Entity
 @Table(name = "people")
 public class Person {
@@ -35,9 +37,23 @@ public class Person {
     @NonNull
     private LocalDate birthDate;
 
+    @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDate createdAt;
 
-    // TODO : Equals & Hashcode
-    // TODO : Life Cycle for createdAt;
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Person person = (Person) o;
+        return getId() != null && Objects.equals(getId(), person.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }

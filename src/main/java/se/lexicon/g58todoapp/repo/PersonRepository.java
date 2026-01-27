a6ed284fd529717400f1cf7d595dcd1788724eb7
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-27 13:54:50 +0100

    Fix derived query

--- src/main/java/se/lexicon/g58todoapp/repo/PersonRepository.java
+++ src/main/java/se/lexicon/g58todoapp/repo/PersonRepository.java
@@ -3,6 +3,7 @@ package se.lexicon.g58todoapp.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.g58todoapp.entity.Person;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
@@ -10,5 +11,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     Optional<Person> findByEmail(String email);
 
     // Is there a person with a specific email?
-    boolean findByEmailExists(String email);
+    boolean existsByEmail(String email);
 }

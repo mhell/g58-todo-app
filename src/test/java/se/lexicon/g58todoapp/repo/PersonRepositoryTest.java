eb9973c8b49e3e8c0fcee86a6f6507511e5378a7
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-28 17:10:07 +0100

    Add PersonRepositoryTest

--- src/test/java/se/lexicon/g58todoapp/repo/PersonRepositoryTest.java
+++ src/test/java/se/lexicon/g58todoapp/repo/PersonRepositoryTest.java
@@ -1,21 +1,42 @@
 package se.lexicon.g58todoapp.repo;
 
-import lombok.Data;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import se.lexicon.g58todoapp.entity.Person;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class PersonRepositoryTest {
+
+    @Autowired
+    PersonRepository personRepository;
+
     @Test
-    void findByEmail() {
+    void findByEmail_exists_returnEntity() {
+        // Arrange
+        String email = "test@test.se";
+        Person person = new Person("Name", email);
+        personRepository.save(person);
+        // Act
+        Optional<Person> foundPerson = personRepository.findByEmail(email);
+        // Assert
+        assertFalse(foundPerson.isEmpty());
+        assertEquals(email, foundPerson.get().getEmail());
     }
 
     @Test
-    void existsByEmail() {
+    void existsByEmail_exists_returnTrue() {
+        // Arrange
+        String email = "test@test.se";
+        Person person = new Person("Name", email);
+        personRepository.save(person);
+        // Act
+        boolean exists = personRepository.existsByEmail(email);
+        // Assert
+        assertTrue(exists);
     }
-
-    //TODO TESTS HERE
-
 }

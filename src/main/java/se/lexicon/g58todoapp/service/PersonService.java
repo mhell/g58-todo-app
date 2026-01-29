f921c3aa6df28dbd47bff05a03814a57beaacc08
Mattias Hellman <mattias.hellman@gmail.com>
2026-01-29 16:50:00 +0100

    Implement PersonService

--- src/main/java/se/lexicon/g58todoapp/service/PersonService.java
+++ src/main/java/se/lexicon/g58todoapp/service/PersonService.java
@@ -7,11 +7,12 @@ import se.lexicon.g58todoapp.repo.PersonRepository;
 import se.lexicon.notify.model.Email;
 import se.lexicon.notify.service.MessageService;
 
+import java.util.List;
+import java.util.Optional;
+
 @Slf4j
 @Service
 public class PersonService {
-
-    // TODO: Expand Service Layer methods
 
     PersonRepository personRepository;
     MessageService<Email> messageService;
@@ -27,17 +28,32 @@ public class PersonService {
             boolean sentMessage = messageService.sendMessage(new Email(
                     person.getEmail(),
                     "Welcome 🫡",
-                    """
+                            """
                             Hello, %s
                             Thank you for signing up to our App.
                             We hope you enjoy using it. 🎉
                             """.formatted(person.getName())));
-
             if (!sentMessage){
                 log.error("Failed to send welcome email to: {}", person.getEmail());
             } else {
                 log.info("Successfully sent welcome email to: {}",person.getEmail());
             }
         }
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+    public Optional<Person> findById(Long id) {
+        return personRepository.findById(id);
+    }
+
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+
+    public Optional<Person> findByEmail(String email) {
+        return personRepository.findByEmail(email);
     }
 }
